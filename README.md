0e4b5ade9e16e17076081a3751389ca255a16e72
Ghadeer Abualrub <ghadeer-96@users.noreply.github.com>
2021-12-18 22:30:09 +0200

    models_deploy

---
+++ README.md
@@ -0,0 +1,11 @@
+# app
+
+This Repository holds an application of trained model to be deployed to production. Using flask python library as a micro web framework to build the backend and HTML/CSS to 
+build frontend, with Heroku as a cloud platform as a service (PaaS) which supporting several programming languages and used to build, run, operate and deploy applications.
+
+app.py : the python file to deal with backend and requests and make predictions.
+templates and static : contain HTML/CSS code to render the GUI of the web application.
+requirement.txt : contains all dependancies and required libraries.
+models : contains all trained models.
+link to the application:
+https://prediction-app-deploy.herokuapp.com 
---
+++ app.py
@@ -0,0 +1,50 @@
+import flask
+import pickle
+import pandas as pd
+import numpy as np
+import sklearn
+
+
+
+# Use pickle to load in the pre-trained model
+model = pickle.load(open(f'models/desicion_tree_model.sav', 'rb'))
+
+
+# Initialise the Flask app
+app = flask.Flask(__name__, template_folder='templates')
+
+
+
+# Set up the main route
+@app.route('/', methods=['GET'])#, 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        # Just render the initial form, to get input
+        return(flask.render_template('home.html'))
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    cols = ['Customer Type', 'Age', 'Type of Travel', 'Class', 'Flight Distance',
+       'Inflight wifi service', 'Departure/Arrival time convenient',
+       'Ease of Online booking', 'Gate location', 'Food and drink',
+       'Online boarding', 'Seat comfort', 'Inflight entertainment',
+       'On-board service', 'Leg room service', 'Baggage handling',
+       'Checkin service', 'Inflight service', 'Cleanliness',
+       'Departure Delay in Minutes', 'Arrival Delay in Minutes']   
+    input = []
+    # Extract the input
+    for i in cols:
+       input.append(flask.request.form[i])
+    
+    final = np.array([input])
+    final = final.astype('int')
+    # Get the model's prediction
+    prediction = model.predict(final)[0]
+    return flask.render_template('home.html',pred='Expected satisfaction {} $'.format(prediction))
+
+    
+
+##################################
+if __name__ == '__main__':
+    app.run()
+
